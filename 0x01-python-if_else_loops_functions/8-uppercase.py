1706068c0946275d0cd187f39f54da32376373a3
tech-gecko <w0nderzz2001@gmail.com>
2023-11-29 19:12:23 -0800

    Update requirement for 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
+    for index, char in enumerate(str):
         if (ord(char) >= 97) and (ord(char) <= 122):
             char = chr(ord(char) - 32)
         print("{}".format(char), end='')
+
+        if index == len(str) - 1:
+            print("\n")
