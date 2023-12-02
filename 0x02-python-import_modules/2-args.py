8249fbfb0b515d22167ea7cdf38a86ac145b04af
tech-gecko <w0nderzz2001@gmail.com>
2023-12-02 14:02:32 -0800

    Update 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    if (len(sys.argv)) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    elif (len(sys.argv)) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        print("{}:".format(i), sys.argv[i])
