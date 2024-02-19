056ea7b137e414ef44cef97bf1e739402a062873
tech-gecko <w0nderzz2001@gmail.com>
2024-02-19 03:13:50 -0800

    Update task 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,4 @@
 -- Script that computes the average of all scores in "second_table".
 -- It also saves this result as a 1-row column called "average".
-SELECT AVG(score) AS average;
+SELECT AVG(score) AS average
+FROM second_table;
