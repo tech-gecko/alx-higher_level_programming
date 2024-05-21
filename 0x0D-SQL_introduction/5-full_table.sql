480061e2afff9e9213dcc5538d171e75aaa0af22
tech-gecko <w0nderzz2001@gmail.com>
2024-05-21 23:08:47 +0100

    Update #4, task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- SQL script that prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
