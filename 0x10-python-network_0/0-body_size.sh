3cf19d464a4871afb7d676b0cabb6deb41038be7
tech-gecko <w0nderzz2001@gmail.com>
2024-07-06 09:24:07 -0700

    Update 1, task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 # Use curl to get the size of the response body in bytes.
-response_size=$(curl -s -o /dev/null -w '%{size_download}' "$1")
-echo $response_size
+curl -s -o /dev/null -w '%{size_download}' "$1"
