c344136609902207f54b2815ee7c1170c0a946a3
tech-gecko <w0nderzz2001@gmail.com>
2024-07-15 00:33:09 +0100

    Corrected some errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 ''' This script fetches URLs from a resource, and returns...
- ...results according to the format required.'''
+ ...results according to the format required.
+'''
 
-if __name__ == __main__: 
+if __name__ == '__main__':
     import urllib.request
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()  
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+         as response:
+        body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
