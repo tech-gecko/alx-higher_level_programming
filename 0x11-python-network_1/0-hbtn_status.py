685715febc7f574d58b633c8e6de8d1589850405
tech-gecko <w0nderzz2001@gmail.com>
2024-07-15 00:16:16 +0100

    Add formatting requirements.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print(html.decode('utf-8'))
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf-8 content: {}".format(body.decode('utf-8')))
