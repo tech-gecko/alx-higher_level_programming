065ed89cf99e809caa1a06373f0b00f7ac9ee19b
tech-gecko <w0nderzz2001@gmail.com>
2024-07-15 00:22:44 +0100

    Remove execution from importation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf-8 content: {}".format(body.decode('utf-8')))
+''' This script fetches URLs from a resource, and returns...
+ ...results according to the format required.'''
+
+if __name__ == __main__: 
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()  
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf-8 content: {}".format(body.decode('utf-8')))
