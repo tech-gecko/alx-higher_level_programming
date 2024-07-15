9511eafa1276849a36dc32d4a593020d22c2383d
tech-gecko <w0nderzz2001@gmail.com>
2024-07-15 15:14:02 -0700

    Add task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+    This script lists 10 commits (from the most recent to oldest)
+    of the repository “rails” by the user “rails”.
+'''
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/' + owner + '/'\
+          + repo + '/' + 'commits'
+    headers = {
+            'accept': 'application/vnd.github+json',
+            'owner': owner,
+            'repo': repo
+            }
+
+    r = requests.get(url, headers=headers)
+    line_no = 0
+
+    for commit in r.json():
+        print("{}: {}".format(commit['sha'], commit['commit']['author']['name']))
+        line_no += 1
+        if line_no == 10:
+            break
