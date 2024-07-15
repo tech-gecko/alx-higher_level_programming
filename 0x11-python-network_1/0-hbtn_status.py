5282cc17b1ff12ece2fb0a2fc1c2408f93cf37e8
tech-gecko <w0nderzz2001@gmail.com>
2024-07-15 04:59:45 -0700

    Update task 0, prolly VSCode error?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-''' This script fetches URLs from a resource, and returns...
- ...results according to the format required.
-'''
+''' This script fetches the url https://alx-intranet.hbtn.io/status
+and displays the body of the response according to the required format.'''
+
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen('https://intranet.hbtn.io/status')\
-         as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(body.decode('utf-8')))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
